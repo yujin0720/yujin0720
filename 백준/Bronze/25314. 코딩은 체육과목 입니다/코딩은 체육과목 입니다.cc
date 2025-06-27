22b35cbccf6ac725ae94986873c425cd03401ce0
yujin Kim <mnmnyujin@naver.com>
2025-06-28 04:48:30 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x ;
+    cin >> x;
+    for(int i = 0 ; i < x/4 ;  i ++)
+        cout << "long" << " ";
+    cout << "int" ; 
+    
+    return 0;
+}
