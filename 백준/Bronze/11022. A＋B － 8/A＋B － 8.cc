57bab6f4ffe499a0bca56f39e81af8e5eff6bf90
yujin Kim <mnmnyujin@naver.com>
2025-06-28 05:08:14 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int x , a,b;
+ cin  >> x;
+ for(int i = 1; i <= x ; i ++)
+ {cin >> a >> b ; 
+  cout << "Case #" << i << ": "<< a << " + " << b << " = " << a +b << "\n";
+  
+ }    
+    return 0;
+}
