57f944b35b7a4f2b2e8345a0053980ec4f3841ac
yujin Kim <mnmnyujin@naver.com>
2025-06-28 04:55:00 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int x ,a,b ;
+ cin >> x;
+ for (int i =1 ; i <= x ; i++)
+   
+ {cin >> a>>b ;
+  cout <<"Case #" << i <<": " << a+b<< "\n" ;
+      }
+
+    
+    return 0;
+}
