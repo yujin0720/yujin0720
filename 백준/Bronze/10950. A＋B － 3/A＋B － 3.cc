d5d97749857e1696b9a3a6f1fc4a3de9861a3135
yujin Kim <mnmnyujin@naver.com>
2025-06-28 04:02:33 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int x , a,b;
+ 
+ cin  >> x;
+ for (int i =0 ; i < x ; i++){
+     cin >> a >> b;
+ cout << a + b << endl; }
+    
+    return 0;
+}
