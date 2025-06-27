a8890626ac5e26e39d2cc63c9a3b3d6453693a4c
yujin Kim <mnmnyujin@naver.com>
2025-06-28 04:41:08 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ 
+    int x ;
+int sum = 0;
+    cin >> x;
+    for(int i =1 ; i <= x ; i++)
+        sum += i;
+        cout << sum;
+    
+    return 0;
+}
